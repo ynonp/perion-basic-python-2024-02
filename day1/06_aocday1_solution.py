d96436a22c6f8c28d38e722990db3e5574a36cb3
ynonp <ynonperek@gmail.com>
2024-02-14 12:45:26 +0200

    add part 2

--- day1/06_aocday1_solution.py
+++ day1/06_aocday1_solution.py
@@ -3,7 +3,16 @@ demo_input = ['1abc2',
               'a1b2c3d4e5f'
               'treb7uchet']
 
+demo_input_2 = ['two1nine',     # 29
+                'eightwothree', # 83
+                'abcone2threexyz', # 13
+                'xtwone3four', # 24
+                '4nineeightseven2', #  42
+                'zoneight234', # 14
+                '7pqrstsixteen'] # 76
+
 # https://github.com/ynonp/perion-basic-python-2024-02
+# https://adventofcode.com/2023/day/1
 
 def fix_calibration_value(value: str) -> int:
     """
