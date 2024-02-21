577c1449f059b8a223a4451387dd7c57962f086a
ynonp-github-demo-tocode <160601725+ynonp-github-demo-tocode@users.noreply.github.com>
2024-02-21 13:30:46 +0200

    day 2

--- day2/07_aoc4.py
+++ day2/07_aoc4.py
@@ -1,12 +1,39 @@
 # https://adventofcode.com/2023/day/4
 
 # 4 Matches: 1 -> 2 -> 4 -> 8
+import io
 
-demo_input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+demo_input = io.StringIO("""Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""")
+
+def card_value(scratchcard):
+    _, numbers = scratchcard.split(':')
+    winning_numbers, my_numbers = numbers.split('|')
+
+    winning_numbers = set([int(x) for x in winning_numbers.split()])
+    my_numbers = set([int(x) for x in my_numbers.split()])
+
+    cross = winning_numbers.intersection(my_numbers)
+    if len(cross) == 0:
+        return 0
+    else:
+        return 2 ** (len(cross) - 1)
+
+
+def part1(data):
+    return sum(card_value(line) for line in data)
+
+
+
+with open('aocday4.txt', encoding='utf8') as f:
+    print(part1(f))
+
+# print(part1(demo_input))
+
+
 
 # find the sum(points_of_all_cards)
---
+++ day2/08_exercises.py
@@ -0,0 +1,5 @@
+# 1. Aoc Day 4 part 2
+# https://adventofcode.com/2023/day/4
+
+# 2. Data Structures from tocode:
+# https://www.tocode.co.il/bundles/python/lessons/19-data-lab?tab=text
